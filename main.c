56c66337a26b5c7135be37a7ccc10a1753fc77f9
angho1230 <angho910@gmail.com>
2023-05-23 20:08:02 +0900

    a

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
 int main(){
-	
+    int a = 0, b = 0;
+	printf("What is your a?\n");
+    scanf("%d", &a);
+    printf("What is your b?\n");
+    scanf("%d", &b);
+    printf("a : %d, b : %d\n", a, b);
 }
